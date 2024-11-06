369ed1b3c35d70d921c44f819676272aabf6c4c5
georgesyrigos <georgesirigosaek@gmail.com>
2024-11-06 13:31:11 +0200

    user page and log out create

--- app/src/main/java/com/example/stylewiz_vol2/UserActivity.java
+++ app/src/main/java/com/example/stylewiz_vol2/UserActivity.java
@@ -2,13 +2,42 @@ package com.example.stylewiz_vol2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        TextView logout = findViewById(R.id.textView11);
+
+        // Retrieve the username from the intent
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("username")) {
+            String username = intent.getStringExtra("username");
+
+            // Display the username in a TextView
+            TextView textViewUsername = findViewById(R.id.textView10);
+            if (textViewUsername != null) {
+                textViewUsername.setText("Welcome " + username+"!");
+            }
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut(); // Sign out the user
+                startActivity(new Intent(UserActivity.this, MainActivity.class)); // Redirect to LoginActivity
+                finish(); // Close current activity
+            }
+        });
     }
 }
