0e014cc84cdf8872a055a76440c8a46bd5493b3c
georgesyrigos <georgesirigosaek@gmail.com>
2024-11-11 20:17:36 +0200

    bottom nav bar readded and new fragments for each page

--- app/src/main/java/com/example/stylewiz_vol2/UserActivity.java
+++ app/src/main/java/com/example/stylewiz_vol2/UserActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -19,7 +22,9 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.HashMap;
 import java.util.Map;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity{
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
 
     private FirestoreHelper firestoreHelper;
     private String user; // Store username as a class-level variable
@@ -33,6 +38,65 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        HomeFragment homeFragment = new HomeFragment();
+        OutfitsFragment outfitsFragment = new OutfitsFragment();
+        NewItemFragment newItemFragment = new NewItemFragment();
+        SuggestionsFragment suggestionsFragment = new SuggestionsFragment();
+
+        frameLayout = findViewById(R.id.frameLayout);
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.bottom_checkroom){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frameLayout,outfitsFragment)
+                            .commit();
+
+                    return true;
+
+                }
+                else if (item.getItemId()==R.id.bottom_home){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frameLayout,homeFragment)
+                            .commit();
+
+                    return true;
+                }
+                else if (item.getItemId()==R.id.bottom_add){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frameLayout,newItemFragment)
+                            .commit();
+
+                    return true;
+                }
+                else if (item.getItemId()==R.id.bottom_suggest){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frameLayout,suggestionsFragment)
+                            .commit();
+
+                    return true;
+                }
+                else {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frameLayout,newItemFragment)
+                            .commit();
+
+                    return true;
+
+                }
+
+
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+
         TextView logout = findViewById(R.id.textView11);
 
         addBtn = findViewById(R.id.buttonAdd);
@@ -90,4 +154,5 @@ public class UserActivity extends AppCompatActivity {
 
 
     }
+
 }
