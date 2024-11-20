ef3227f76989604d9b0aa5e4dd2d32bb2deea729
georgesyrigos <georgesirigosaek@gmail.com>
2024-11-20 16:25:35 +0200

    new item fragment addition image from storage and redesign

--- app/src/main/java/com/example/stylewiz_vol2/NewItemFragment.java
+++ app/src/main/java/com/example/stylewiz_vol2/NewItemFragment.java
@@ -1,20 +1,35 @@
 package com.example.stylewiz_vol2;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
 
 public class NewItemFragment extends Fragment {
 
@@ -24,6 +39,10 @@ public class NewItemFragment extends Fragment {
     Button newItem;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    private MaterialCardView selectPhoto;
+    private ImageView ItemImageView;
+    private Uri ImageUri;
+    private Bitmap bitmap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,7 +54,19 @@ public class NewItemFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
+        //Add image on click
+        selectPhoto = view.findViewById(R.id.selectImage);
+        ItemImageView = view.findViewById(R.id.itemImage);
+        selectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check permission
+                CheckStoragePermission();
 
+                PickFromGallery();
+
+            }
+        });
 
         // Initialize Firebase instances
         fAuth = FirebaseAuth.getInstance();
@@ -81,4 +112,69 @@ public class NewItemFragment extends Fragment {
         });
 
     }
+
+    private void CheckStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ContextCompat.checkSelfPermission(getActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            }else {
+                //method to pick image from gallery
+                PickFromGallery();
+            }
+        }else {
+            //method to pick image from gallery
+            PickFromGallery();
+        }
+    }
+
+    private void PickFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        //call launcer
+        launcher.launch(intent);
+    }
+    ActivityResultLauncher<Intent> launcher
+            =registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                        if (result.getResultCode()== Activity.RESULT_OK){
+                            Intent data=result.getData();
+                            if(data!= null && data.getData()!=null){
+
+                                ImageUri=data.getData();
+
+                                //convert image to bitmap
+                                try {
+                                    bitmap= MediaStore.Images.Media.getBitmap(
+                                            getActivity().getContentResolver(),
+                                            ImageUri
+                                    );
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+
+                            }
+                            //set image into imageview
+                            if (ImageUri!=null){
+                                ItemImageView.setImageBitmap(bitmap);
+
+                            }
+
+
+
+                        }
+
+            }
+
+    );
+
+    //upload image into Firebase storage in store Image Url into Firebase firestore
+
+    //make a method to Upload Image into firebase storage
+
+
+
 }
