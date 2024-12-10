fcb0052f82355b2a018df693b5870c78908a4bf8
georgesyrigos <georgesirigosaek@gmail.com>
2024-12-10 11:52:35 +0200

    username in home fragment shown with FirebaseAuth through email

--- app/src/main/java/com/example/stylewiz_vol2/HomeFragment.java
+++ app/src/main/java/com/example/stylewiz_vol2/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.stylewiz_vol2;
 
+import static android.app.ProgressDialog.show;
+
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -11,7 +13,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
     TextView textViewUsername;
@@ -31,16 +38,44 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        showUserData(view);
 
-        // Retrieve the username from the Intent passed to the Activity
-        Intent intent = getActivity().getIntent();
-        if (intent != null && intent.hasExtra("username")) {
-            user = intent.getStringExtra("username");
-            // Display the username in a TextView
-            textViewUsername = view.findViewById(R.id.homeFragment);
-            if (textViewUsername != null) {
-                textViewUsername.setText("Welcome " + user +"!");
+
+    }
+
+    private void showUserData(View view) {
+        textViewUsername = view.findViewById(R.id.homeFragment);
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirestoreHelper firestoreHelper = new FirestoreHelper();
+
+
+        if (user != null) {
+            String email = user.getEmail();
+            if (email!=null){
+                firestoreHelper.getUsername(email, new FirestoreHelper.UsernameCallback() {
+                    @Override
+                    public void onSuccess(String username) {
+                        textViewUsername.setText("Welcome "+ username + "!");
+
+                    }
+
+                    @Override
+                    public void onFailure(String errorMessage) {
+                        textViewUsername.setText("Unknown User");
+                        Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+
+            } else {
+                Toast.makeText(getActivity(), "User not authenticated", Toast.LENGTH_SHORT).show();
             }
+        } else {
+            Toast.makeText(getActivity(), "No email found", Toast.LENGTH_SHORT).show();
         }
 
 
