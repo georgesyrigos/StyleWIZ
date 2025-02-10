5e0ccbe34db92736c2f306da9751b4dced31dccd
georgesyrigos <georgesirigosaek@gmail.com>
2025-02-10 18:53:44 +0200

    first development of outfits fragment

--- app/src/main/java/com/example/stylewiz_vol2/OutfitsFragment.java
+++ app/src/main/java/com/example/stylewiz_vol2/OutfitsFragment.java
@@ -2,20 +2,56 @@ package com.example.stylewiz_vol2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OutfitsFragment extends Fragment {
 
-
+    private LinearLayout emptyStateLayout;
+    GridView gridView;
+    ItemsAdapter adapter;
+    List<DataClass> dataList;
+    FirebaseFirestore db;
+    private boolean isManualUpdate = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_outfits, container, false);
+        View view = inflater.inflate(R.layout.fragment_outfits, container, false);
+
+
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+
+
 }
