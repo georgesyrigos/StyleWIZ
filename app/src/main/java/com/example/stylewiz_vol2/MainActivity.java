64054d6a5739a3b9dd91cea07658155d7f0974ef
georgesyrigos <georgesirigosaek@gmail.com>
2025-07-30 13:36:18 +0300

    app icon and name changed and small changes to sign up, sign in

--- app/src/main/java/com/example/stylewiz_vol2/MainActivity.java
+++ app/src/main/java/com/example/stylewiz_vol2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.stylewiz_vol2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     String userID;
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/stylewiz_vol2/SignUpActivity.java
+++ app/src/main/java/com/example/stylewiz_vol2/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.stylewiz_vol2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -37,6 +38,7 @@ public class SignUpActivity extends AppCompatActivity {
 
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
